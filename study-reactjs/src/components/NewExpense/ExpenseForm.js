cd6469371c1cb80ecf53575eb3a61097c1b8ee7a
1000yj <ssaltuck@gmail.com>
2021-10-27 07:37:58 +0900

    Dynamic styling
    - 각각의 요소 제어하는 것이 아니라 하나의 요소만 제어하여 개선 여지 있음

--- study-reactjs/src/components/NewExpense/ExpenseForm.js
+++ study-reactjs/src/components/NewExpense/ExpenseForm.js
@@ -3,95 +3,103 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = (props) => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enteredDate, setEnteredDate] = useState('');
-  // const [userInput, setUserInput] = useState({
-  //   enteredTitle: '',
-  //   enteredAmount: '',
-  //   enteredDate: '',
-  // });
-
-  const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredTitle: event.target.value,
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //   enteredTitle: '',
+    //   enteredAmount: '',
+    //   enteredDate: '',
     // });
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: event.target.value };
-    // });
-  };
 
-  const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredAmount: event.target.value,
-    // });
-  };
+    const [isValid, setIsValid] = useState(true);
 
-  const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
-    // setUserInput({
-    //   ...userInput,
-    //   enteredDate: event.target.value,
-    // });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    const expenseData = {
-      title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate),
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        // setUserInput({
+        //   ...userInput,
+        //   enteredTitle: event.target.value,
+        // });
+        // setUserInput((prevState) => {
+        //   return { ...prevState, enteredTitle: event.target.value };
+        // });
     };
 
-    props.onSaveExpenseData(expenseData);
-    setEnteredTitle('');
-    setEnteredAmount('');
-    setEnteredDate('');
-  };
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //   ...userInput,
+        //   enteredAmount: event.target.value,
+        // });
+    };
 
-  return (
-    <form onSubmit={submitHandler}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Title</label>
-          <input
-            type='text'
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
-        </div>
-        <div className='new-expense__control'>
-          <label>Amount</label>
-          <input
-            type='number'
-            min='0.01'
-            step='0.01'
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
-        </div>
-        <div className='new-expense__control'>
-          <label>Date</label>
-          <input
-            type='date'
-            min='2019-01-01'
-            max='2022-12-31'
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className='new-expense__actions'>
-        <button type="button" onClick={props.onCancel}>Cancel</button>
-        <button type='submit'>Add Expense</button>
-      </div>
-    </form>
-  );
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //   ...userInput,
+        //   enteredDate: event.target.value,
+        // });
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        if (enteredTitle.trim().length === 0 || enteredDate.trim().length === 0 || enteredAmount.trim().length === 0) {
+            alert("빈 칸은 허용되지 않습니다.");
+            setIsValid(false);
+            return 0;
+        }
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+
+        props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className='new-expense__controls'>
+                <div className={`new-expense__control ${!isValid? 'invalid' : ''}`}>
+                    <label>Title</label>
+                    <input
+                        type='text'
+                        value={enteredTitle}
+                        onChange={titleChangeHandler}
+                    />
+                </div>
+                <div className={`new-expense__control ${!isValid ? 'invalid' : ''}`}>
+                    <label>Amount</label>
+                    <input
+                        type='number'
+                        min='0.01'
+                        step='0.01'
+                        value={enteredAmount}
+                        onChange={amountChangeHandler}
+                    />
+                </div>
+                <div className={`new-expense__control &${!isValid? 'invalid' : ''}`}>
+                    <label>Date</label>
+                    <input
+                        type='date'
+                        min='2019-01-01'
+                        max='2022-12-31'
+                        value={enteredDate}
+                        onChange={dateChangeHandler}
+                    />
+                </div>
+            </div>
+            <div className='new-expense__actions'>
+                <button type="button" onClick={props.onCancel}>Cancel</button>
+                <button type='submit'>Add Expense</button>
+            </div>
+        </form>
+    );
 };
 
 export default ExpenseForm;
